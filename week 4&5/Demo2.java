270cfbab9b4495eeb98631047895177f5d660ffd
saikiranjetta <saikiranjetta777@gmail.com>
2026-03-04 07:09:51 +0000

    Add files via upload

---
+++ week 4&5/Demo2.java
@@ -0,0 +1,11 @@
+package p2;
+import p2.protection2;
+import p2.OtherPackage;
+public class Demo2 
+{
+	public static void main(String args[]) 
+	{
+		protection2 ob1 = new protection2();
+		OtherPackage ob2 = new OtherPackage();
+	}
+}
---
+++ week 4&5/protection.java
@@ -0,0 +1,16 @@
+package p1;
+public class protection
+{
+	int n=1;
+	private int n_pri=2;
+	public int n_pub=4;
+	protected int n_pro=3;
+	public protection()
+	{
+		System.out.println("base constructor");
+		System.out.println("n= "+n);
+		System.out.println("n_pri= "+n_pri);
+		System.out.println("n_pub= "+n_pub);
+		System.out.println("n_pro= "+n_pro);
+	}
+}
